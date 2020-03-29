c668317db24f94b5113969ac0be00d322a8f18aa
Veronica <veronicasrocha@gmail.com>
2020-03-29 15:55:26 -0300

    Fase Inicial do Mobile

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Incidents() {
+    return (
+        <View>
+            <Text>OI</Text>
+        </View>
+        
+    );
+}
